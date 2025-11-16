ecaaa86b3d67270f5244a59213182ce719fd7ff0
Exiivy <jscadman3564@gmail.com>
2025-11-16 21:52:52 +0900

    feat: add toon shading project

---
+++ 08_toon_shading/shFrag.glsl
@@ -0,0 +1,64 @@
+#version 300 es
+
+precision highp float;
+
+out vec4 FragColor;
+in vec3 fragPos;  
+in vec3 normal;  
+in vec2 texCoord;
+
+struct Material {
+    vec3 diffuse; // diffuse map
+    vec3 specular;     // 표면의 specular color
+    float shininess;   // specular 반짝임 정도
+};
+
+struct Light {
+    //vec3 position;
+    vec3 direction;
+    vec3 ambient; // ambient 적용 strength
+    vec3 diffuse; // diffuse 적용 strength
+    vec3 specular; // specular 적용 strength
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 u_viewPos;
+uniform int u_toonLevel;
+
+float quanti(float n){
+    float toonUnit = 1.0 / float(u_toonLevel);
+    for(int i = 1; i <= u_toonLevel; i++){
+        if(n <= toonUnit * float(i)){
+            return toonUnit * (float(i) - 1.0);
+        }
+    }
+    return toonUnit * (float(u_toonLevel) - 1.0); // failsafe
+}
+
+void main() {
+    // ambient
+    vec3 rgb = material.diffuse;
+    vec3 ambient = light.ambient * rgb;
+  	
+    // diffuse 
+    vec3 norm = normalize(normal);
+    //vec3 lightDir = normalize(light.position - fragPos);
+    vec3 lightDir = normalize(light.direction);
+    float dotNormLight = dot(norm, lightDir);
+    float diff = quanti(max(dotNormLight, 0.0));
+    vec3 diffuse = light.diffuse * diff * rgb;  
+    
+    // specular
+    vec3 viewDir = normalize(u_viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = 0.0;
+    if (dotNormLight > 0.0) {
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        spec = quanti(spec);
+    }
+    vec3 specular = light.specular * spec * material.specular;  
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+} 
